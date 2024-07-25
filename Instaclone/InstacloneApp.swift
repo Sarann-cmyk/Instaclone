44a47c69901f2cd52d8535ac06d612ea99e635af
Alex Synelnyk <xalev@ukr.net>
2024-07-25 13:28:45 +0300

    Initial Commit

---
+++ Instaclone/InstacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstacloneApp.swift
+//  Instaclone
+//
+//  Created by Aleks Synelnyk on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

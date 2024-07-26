63b093b426ce70bfb9673bdbefc03045aab39726
Alex Synelnyk <xalev@ukr.net>
2024-07-26 15:09:48 +0300

    Clining

--- Instaclone/ContentView.swift
+++ Instaclone/ContentView.swift
@@ -10,16 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
+
             Text("Hello, world!")
         }
         .padding()

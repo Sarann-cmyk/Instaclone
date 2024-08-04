d07bfca95afe92fb8bd3c1edcdd92affafdb5b14
Alex Synelnyk <xalev@ukr.net>
2024-08-04 18:36:32 +0300

    002 Creation StoryCollectionView

--- Instaclone/ContentView.swift
+++ Instaclone/ContentView.swift
@@ -9,46 +9,66 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        navigationBar
+        VStack {
+            navigationBar
+        }
     }
-        
-        var navigationBar: some View {
-            HStack {
-                Image("instagram")
-                    .resizable()
-                    .frame(width:  165, height: 65)
+    
+    var navigationBar: some View {
+        HStack {
+            Image("instagram")
+                .resizable()
+                .frame(width:  165, height: 65)
+            
+            Spacer()
+            
+            HStack(spacing: 26) {
                 
-                Spacer()
                 
-                HStack(spacing: 26) {
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "plus.app")
                     
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Image(systemName: "plus.app")
-                        
-                    }
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Image(systemName: "heart")
-                        
-                    }
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Image(systemName: "paperplane")
-                        
-                    }
                 }
-                .foregroundColor(.black)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "heart")
+                    
+                }
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "paperplane")
+                    
+                }
             }
-            .padding(.horizontal, 20)
+            .foregroundColor(.black)
+        }
+        .padding(.horizontal, 20)
+    }
+}
+    
+    
+    struct StoryCollectionView: View {
+        var body: some View {
+            ZStack {
+                Circle()
+                    .border(Color.white, width: 2)
+            Image("001")
+                .resizable()
+                .clipped()
+                .border(Color.white, width: 2)
+                .scaledToFill()
+                .cornerRadius(100)
+        }
+            .frame(width: 66, height: 66)
+    }
+}
+    
+    struct ContentView_Previews:  PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .previewDevice("iPhone 13")
+            StoryCollectionView()
+                .previewDevice("iPhone 13")
         }
     }
 
-
-
-struct ContentView_Previews:  PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .previewDevice("iPhone 13")
-    }
-}
